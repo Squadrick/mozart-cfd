8766494f9eca37c20dbb5609fc229ad456080f01
Dheeraj R Reddy <dheeraj98reddy@gmail.com>
2018-01-22 22:48:57 +0530

    Added music generation scripts
    A script that converts an audio clip to the MIDI equivalent using the Melodia algorithm
    Two trained models(Magenta) -- regular LSTM, attention LSTM
    A basic prediction script that uses the above models for MIDI generation

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # mozart-cfd
 App by team Hail Hydra for Microsoft code.fun.do 2017
+
+Listens to acoustic audio from the mic, converts it to MIDI and uses an attention LSTM to generate a corresponding duet which is then played back to the user.
---
+++ music_generation/audio_to_midi_melodia.py
@@ -0,0 +1,151 @@
+from __future__ import print_function
+
+import librosa
+import vamp
+import argparse
+import os
+import numpy as np
+from midiutil.MidiFile import MIDIFile
+from scipy.signal import medfilt
+
+def save_midi(outfile, notes, tempo):
+
+    track = 0
+    time = 0
+    midifile = MIDIFile(1)
+
+    # Add track name and tempo.
+    midifile.addTrackName(track, time, "MIDI TRACK")
+    midifile.addTempo(track, time, tempo)
+
+    channel = 0
+    volume = 100
+
+    for note in notes:
+        onset = note[0] * (tempo/60.)
+        duration = note[1] * (tempo/60.)
+        # duration = 1
+        pitch = note[2]
+        midifile.addNote(track, channel, pitch, onset, duration, volume)
+
+    # And write it to disk.
+    binfile = open(outfile, 'wb')
+    midifile.writeFile(binfile)
+    binfile.close()
+
+
+def midi_to_notes(midi, fs, hop, smooth, minduration):
+
+    # smooth midi pitch sequence first
+    if (smooth > 0):
+        filter_duration = smooth  # in seconds
+        filter_size = int(filter_duration * fs / float(hop))
+        if filter_size % 2 == 0:
+            filter_size += 1
+        midi_filt = medfilt(midi, filter_size)
+    else:
+        midi_filt = midi
+    # print(len(midi),len(midi_filt))
+
+    notes = []
+    p_prev = None
+    duration = 0
+    onset = 0
+    for n, p in enumerate(midi_filt):
+        if p == p_prev:
+            duration += 1
+        else:
+            # treat 0 as silence
+            if p_prev > 0:
+                # add note
+                duration_sec = duration * hop / float(fs)
+                # only add notes that are long enough
+                if duration_sec >= minduration:
+                    onset_sec = onset * hop / float(fs)
+                    notes.append((onset_sec, duration_sec, p_prev))
+
+            # start new note
+            onset = n
+            duration = 1
+            p_prev = p
+
+    # add last note
+    if p_prev > 0:
+        # add note
+        duration_sec = duration * hop / float(fs)
+        onset_sec = onset * hop / float(fs)
+        notes.append((onset_sec, duration_sec, p_prev))
+
+    return notes
+
+
+def hz2midi(hz):
+
+    # convert from Hz to midi note
+    hz_nonneg = hz.copy()
+    hz_nonneg[hz <= 0] = 0
+    midi = 69 + 12*np.log2((hz_nonneg/440.) + 1e-5)
+    midi[midi <= 0] = 0
+    
+    # round
+    midi = np.round(midi)
+    
+    return midi
+
+
+def audio_to_midi_melodia(infile, outfile, smooth=0.25, minduration=0.0):
+    # define analysis parameters
+    fs = 44100
+    hop = 128
+
+    # load audio using librosa
+    print("Loading audio...")
+    data, sr = librosa.load(infile, sr=fs, mono=True)
+
+    onset_env = librosa.onset.onset_strength(data, sr=sr)
+    bpm = librosa.beat.estimate_tempo(onset_env, sr=sr)
+    print("Estimated bpm: ", bpm)
+    # extract melody using melodia vamp plugin
+    print("Extracting melody f0 with MELODIA...")
+    melody = vamp.collect(data, sr, "mtg-melodia:melodia",
+                          parameters={"voicing": 0.2})
+    
+    # hop = melody['vector'][0]
+    pitch = melody['vector'][1]
+    # impute missing 0's to compensate for starting timestamp
+    pitch = np.insert(pitch, 0, [0]*8)
+    
+    print(pitch)
+    
+    # debug
+    # np.asarray(pitch).dump('f0.npy')
+    # print(len(pitch))
+
+    # convert f0 to midi notes
+    print("Converting Hz to MIDI notes...")
+    midi_pitch = hz2midi(pitch)
+
+    # segment sequence into individual midi notes
+    notes = midi_to_notes(midi_pitch, fs, hop, smooth, minduration)
+
+    # save note sequence to a midi file
+    print("Saving MIDI to disk...")
+    save_midi(outfile, notes, bpm)
+
+    print("Conversion complete.")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("infile", help="Path to input audio file.")
+    parser.add_argument("outfile", help="Path for saving output MIDI file.")
+    parser.add_argument("--smooth", type=float, default=0.25,
+                        help="Smooth the pitch sequence with a median filter "
+                             "of the provided duration (in seconds).")
+    parser.add_argument("--minduration", type=float, default=0.0,
+                        help="Minimum allowed duration for note (in seconds). "
+                             "Shorter notes will be removed.")
+
+    args = parser.parse_args()
+    audio_to_midi_melodia(args.infile, args.outfile, 
+                          smooth=args.smooth, minduration=args.minduration)
